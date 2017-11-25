6b733baa3a7a6448daafa68b7a0b7478e94fac79
Chuck Stietzel <cstietzel@yahoo.com>
2017-11-25 11:46:51 -0500

    Add Shiny application and Rmd preso

---
+++ wk4-app/HSApp/server.R
@@ -0,0 +1,147 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+library(shiny)
+library(tidyverse)
+library(readxl)
+library(plotly)
+library(DT)
+
+options(warn =-1)
+
+## Get Absenteeism data
+absent <- data_frame()
+files <- list.files(path="~/EdDataCT", pattern = "chronicAbsenteeism1", full.names = TRUE)
+for (f in files) {
+  flen <- nchar(f)
+  tempdf = read_csv(f, skip = 4, na = "*",
+                    col_names  = c("DistrictName", "DistrictCode", "SchoolName", "SchoolCode", "PctAbsent"),
+                    col_types = cols(
+                      DistrictName = col_character(),
+                      DistrictCode = col_integer(),
+                      SchoolName = col_character(),
+                      SchoolCode = col_integer(),
+                      PctAbsent = col_double()
+                    )) %>% 
+    mutate(SchoolYear = substr(f, flen - 8,flen - 4)) %>%
+    select(SchoolYear, SchoolName, everything())
+  
+  absent <-bind_rows(absent, tempdf) 
+}
+
+## Get Per Pupil Spending data
+spending <- data_frame()
+files <- list.files(path="~/EdDataCT", pattern = "perPupilExpeditures", full.names = TRUE)
+for (f in files) {
+  flen <- nchar(f)
+  tempdf <- read_csv(f, skip = 5, na = "N/A", 
+                     col_names  = c("DistrictName", "TeachingStaff", "TeachingSupplies", "MediaServices", "StudentSupport", 
+                                    "Administration", "Facilities", "Transportation", "Other", "TotalSpend")) %>%
+    mutate(SchoolYear = substr(f, flen - 8,flen - 4)) %>%
+    filter_all(all_vars(!is.na(.)))
+  
+  spending <-bind_rows(spending, tempdf) 
+}
+
+## Get School Enrollment Data
+enrollment <- data_frame()
+files <- list.files(path="~/EdDataCT", pattern = "enrollmentSingleYear", full.names = TRUE)
+for (f in files) {
+  flen <- nchar(f)
+  tempdf <- read_csv(f, skip = 6, na = "*",
+                     col_names  = c("DistrictName", "DistrictCode", "SchoolName", "SchoolCode", "NativeAmerican", 
+                                    "Asian", "Black", "Hispanic", "Islander", "Mixed", "White", "Total")) %>% 
+    mutate(SchoolYear = substr(f, flen - 8,flen - 4)) %>%
+    select(SchoolCode, SchoolYear, Enrolled = Total)
+  
+  
+  enrollment <-bind_rows(enrollment, tempdf) 
+}
+
+## Combine datasets together and calculate 4-year averages
+hsdata <- inner_join(absent, enrollment, by = c("SchoolCode", "SchoolYear")) %>%  
+  inner_join(spending, by = c("DistrictName", "SchoolYear")) %>%
+  group_by(SchoolName, SchoolCode, DistrictName, DistrictCode) %>%
+  ## Use schools that have data for all four years
+  filter(n() == 4) %>% select(-SchoolYear) %>%
+  ## Calculate the 4-year mean values for all variables
+  summarize_all(function (x) {signif(mean(x, na.rm=TRUE), 4)})
+
+## Load graduation and college entry rates for class of 2015
+results <- read_xlsx(path="~/EdDataCT/nextgenresults15-16.xlsx", sheet = 3) %>%
+  filter(grepl("School", SchoolOrgType) & !is.na(Ind8Rate) & 
+           SchoolLowGrade == 9 & SchoolHighGrade == 12) %>%
+  select(SchoolCode, SchoolOrgType, GradRate = Ind8Rate, CollegeRate = Ind10Rate) %>%
+  mutate(SchoolCode = as.integer(SchoolCode), 
+         GradRate = round(100*GradRate, 1), CollegeRate = round(100*CollegeRate, 1)) 
+
+hsdatasum <- inner_join(results, hsdata, by = "SchoolCode") %>%
+  select(SchoolName, DistrictName, GradRate, CollegeRate, Enrolled, PctAbsent, TotalSpend)
+
+shinyServer(function(input, output) {
+  
+  fit <- reactive({
+    Formula <- as.formula(paste(input$outcome, "~", input$indepvar))
+    fit <- lm(data = hsdatasum, Formula)
+    names(fit$coefficients) <- c("Intercept", input$indepvar)
+    fit
+  })
+    
+  
+  # Regression output
+  output$summary <- renderPrint({
+    print(paste("Formula:", input$outcome, "~", input$indepvar))
+    summary(fit())
+  })
+  
+  # Data output
+  output$tbl = renderDataTable({
+    datatable(hsdatasum, options = list(lengthChange = FALSE), rownames = FALSE)
+  })
+  
+  
+  # Scatterplot output
+  output$scatterplot1 <- renderPlotly({
+   fmla.x <- as.formula(paste("~", input$indepvar))
+   fmla.y <- as.formula(paste("~", input$outcome))
+   loess1 <- loess.smooth(as.data.frame(hsdatasum[,input$indepvar]), 
+                          as.data.frame(hsdatasum[,input$outcome]))
+   
+   plot_ly(hsdatasum, x = fmla.x, y = fmla.y, type = "scatter", mode = "markers",
+           text = ~SchoolName, marker = list(size = 10, opacity = 0.5)) %>%
+     layout(title = paste(input$indepvar, "vs", input$outcome),
+            xaxis = list(title = input$indepvar),
+            yaxis = list(title = input$outcome)) %>%
+     add_trace(y = fitted(fit()), type = "scatter", mode = "line", name = "Smooth",
+             line = list(width = 2))
+  })
+  
+  output$scatterplot <- renderPlotly({
+    p <- ggplot(hsdatasum, aes_string(x=input$indepvar, y=input$outcome)) + 
+     geom_point(size = 2, alpha = 0.5) + geom_smooth(method="lm", col="red", size=0.5) +
+    xlab(input$indepvar) + ylab(input$outcome)
+   ggplotly(p)
+    
+    })
+  
+  
+  # Histogram output var 1
+  output$distribution1 <- renderPlotly({
+    fmla.x <- as.formula(paste("~", input$indepvar))
+    plot_ly(hsdatasum, x = fmla.x, type = "histogram") %>%
+      layout(title = input$indepvar)
+  })
+  
+  # Histogram output var 2
+  output$distribution2 <- renderPlotly({
+    fmla.y <- as.formula(paste("~", input$outcome))
+    plot_ly(hsdatasum, x = fmla.y, type = "histogram") %>%
+      layout(title = input$outcome)
+  })
+  
+})
---
+++ wk4-app/HSApp/ui.R
@@ -0,0 +1,47 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(plotly)
+library(DT)
+
+
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  titlePanel("2015 Connecticut Public High School Performance"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("outcome", label = h4("Class of 2015 Result"),
+                  choices = list("Graduation Rate" = "GradRate",
+                                 "College Entry Rate" = "CollegeRate"), selected = 1),
+      selectInput("indepvar", label = h4("4-Year Average Data"),
+                  choices = list("Absentee Rate" = "PctAbsent",
+                                 "Spend Per Pupil ($)" = "TotalSpend",
+                                 "School Enrollment" = "Enrolled"), selected = 1)
+      
+    ),
+    
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  tabPanel("Instructions", includeHTML("instructions.html")),
+          
+                  tabPanel("Scatterplot", br(), plotlyOutput("scatterplot")), # Plot
+                  tabPanel("Distribution", br(), # Plots of distributions
+                           fluidRow(
+                             column(6, plotlyOutput("distribution1")),
+                             column(6, plotlyOutput("distribution2")))
+                  ),
+                  tabPanel("Model Summary", verbatimTextOutput("summary")), # Regression output
+                  tabPanel("Data", div(dataTableOutput('tbl'), style = "font-size:80%")) # Data as datatable
+
+      )
+    )
+  ))
+  )
