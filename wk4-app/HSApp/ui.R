0dcfab986d338cd59fc781ebd937b354f4a50b21
Chuck Stietzel <cstietzel@yahoo.com>
2017-11-26 11:40:55 -0500

    Add data to project; Added instructions tab to app; Updated slides.

--- wk4-app/HSApp/ui.R
+++ wk4-app/HSApp/ui.R
@@ -1,17 +1,6 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(plotly)
 library(DT)
-
-
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
